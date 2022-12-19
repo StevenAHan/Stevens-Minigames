176c5f8f67a2ac119610c7a0f3af2a845aa557e5
majikyouser <sahiagb@gmail.com>
2022-12-19 13:12:44 -0500

    Finished base spellcast solver algorithm

--- spellcastSolver.cpp
+++ spellcastSolver.cpp
@@ -13,6 +13,8 @@ Made By: Steven Han
 #include <fstream>
 using namespace std;
 
+// Define the character size
+#define CHAR_SIZE 128
 
 /*
     The cell is what will contain the letter and its respective point value.
@@ -55,10 +57,151 @@ vector<vector<Cell>> convertBoard(const string& board)
     return vec;
 }
 
+// A class to store a Trie node
+class Trie
+{
+public:
+    bool isLeaf;
+    Trie* character[CHAR_SIZE];
+ 
+    // Constructor
+    Trie()
+    {
+        this->isLeaf = false;
+ 
+        for (int i = 0; i < CHAR_SIZE; i++) {
+            this->character[i] = nullptr;
+        }
+    }
+ 
+    void insert(string);
+    bool deletion(Trie*&, string);
+    bool search(string);
+    bool haveChildren(Trie const*);
+};
+ 
+// Iterative function to insert a key into a Trie
+void Trie::insert(string key)
+{
+    // start from the root node
+    Trie* curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // create a new node if the path doesn't exist
+        if (curr->character[key[i]] == nullptr) {
+            curr->character[key[i]] = new Trie();
+        }
+ 
+        // go to the next node
+        curr = curr->character[key[i]];
+    }
+ 
+    // mark the current node as a leaf
+    curr->isLeaf = true;
+}
+ 
+// Iterative function to search a fragment of a word in the trie. If it exists, return true.
+bool Trie::search(string key)
+{
+    // return false if Trie is empty
+    if (this == nullptr) {
+        return false;
+    }
+ 
+    Trie* curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // go to the next node
+        curr = curr->character[key[i]];
+ 
+        // if the string is invalid (reached end of a path in the Trie)
+        if (curr == nullptr) {
+            return false;
+        }
+    }
+ 
+    // return true if the current node exists
+    return curr;
+}
+ 
+// Returns true if a given node has any children
+bool Trie::haveChildren(Trie const* curr)
+{
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        if (curr->character[i]) {
+            return true;    // child found
+        }
+    }
+ 
+    return false;
+}
+ 
+// Recursive function to delete a key in the Trie
+bool Trie::deletion(Trie*& curr, string key)
+{
+    // return if Trie is empty
+    if (curr == nullptr) {
+        return false;
+    }
+ 
+    // if the end of the key is not reached
+    if (key.length())
+    {
+        // recur for the node corresponding to the next character in the key
+        // and if it returns true, delete the current node (if it is non-leaf)
+ 
+        if (curr != nullptr &&
+            curr->character[key[0]] != nullptr &&
+            deletion(curr->character[key[0]], key.substr(1)) &&
+            curr->isLeaf == false)
+        {
+            if (!haveChildren(curr))
+            {
+                delete curr;
+                curr = nullptr;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+ 
+    // if the end of the key is reached
+    if (key.length() == 0 && curr->isLeaf)
+    {
+        // if the current node is a leaf node and doesn't have any children
+        if (!haveChildren(curr))
+        {
+            // delete the current node
+            delete curr;
+            curr = nullptr;
+ 
+            // delete the non-leaf parent nodes
+            return true;
+        }
+ 
+        // if the current node is a leaf node and has children
+        else {
+            // mark the current node as a non-leaf node (DON'T DELETE IT)
+            curr->isLeaf = false;
+ 
+            // don't delete its parent nodes
+            return false;
+        }
+    }
+ 
+    return false;
+}
+
+
 /*
     Helper used to find the largest word recursively, and generate a list of valid words.
 */
-pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered_set<string>& dictionary, vector<vector<bool>>& alreadyUsed, int row, int col, string prevWord, int prevVal, unordered_map<string,int>& listOfValids)
+pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered_set<string>& dictionary, 
+    vector<vector<bool>>& alreadyUsed, int row, int col, string prevWord, int prevVal, unordered_map<string,int>& listOfValids, 
+    Trie* head)
 {
     string currWord = prevWord + board[row][col].letter;
     int currVal = prevVal + board[row][col].points;
@@ -69,12 +212,17 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
         listOfValids[currWord] = currVal;
         currMax = make_pair(currVal, currWord);
     }
-
+    // If the word fragment doesn't exist in the trie, return the current max early
+    if(!(head->search(currWord)))
+    {
+        return currMax;
+    }
     alreadyUsed[row][col] = true;
     // right
     if(col < board[row].size() - 1 && alreadyUsed[row][col + 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row, col + 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row, col + 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -83,7 +231,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // left
     if(col > 0 && alreadyUsed[row][col - 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row, col - 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row, col - 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -92,7 +241,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // up
     if(row < board.size() - 1 && alreadyUsed[row + 1][col] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -101,7 +251,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // down
     if(row > 0 && alreadyUsed[row - 1][col] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -110,7 +261,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // up right
     if(col < board[row].size() - 1 && row < board.size() - 1 && alreadyUsed[row + 1][col + 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col + 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col + 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -119,7 +271,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // down right
     if(col < board[row].size() - 1 && row > 0 && alreadyUsed[row - 1][col + 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col + 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col + 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -128,7 +281,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // up left
     if(col > 0 && row < board.size() - 1 && alreadyUsed[row + 1][col - 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col - 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row + 1, col - 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -137,7 +291,8 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
     // down left
     if(col > 0 && row > 0 && alreadyUsed[row - 1][col - 1] == false)
     {
-        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col - 1, currWord, currVal, listOfValids);
+        pair<int,string> tempVal = solverHelper(board, dictionary, alreadyUsed, row - 1, col - 1, 
+            currWord, currVal, listOfValids, head);
         if(currMax.first < tempVal.first)
         {
             currMax = tempVal;
@@ -150,7 +305,7 @@ pair<int,string> solverHelper(const vector<vector<Cell>>& board, const unordered
 /*
     Primary Solver Function. Returns the largest word and its point value given the board and a dictionary of valid words.
 */
-pair<int,string> solver(const vector<vector<Cell>>& board, const unordered_set<string>& dictionary)
+pair<int,string> solver(const vector<vector<Cell>>& board, const unordered_set<string>& dictionary, Trie* head)
 {
     // Used to keep track of if the letter is already being used for the word
     vector<vector<bool>> alreadyUsed(board.size(), vector<bool>(board[0].size(), false));
@@ -163,7 +318,7 @@ pair<int,string> solver(const vector<vector<Cell>>& board, const unordered_set<s
         for(int col = 0; col < board[row].size(); col++)
         {
             alreadyUsed = vector<vector<bool>>(board.size(), vector<bool>(board[0].size(), false));
-            pair<int,string> tempBest = solverHelper(board, dictionary, alreadyUsed, row, col, "" , 0, listOfValids);
+            pair<int,string> tempBest = solverHelper(board, dictionary, alreadyUsed, row, col, "" , 0, listOfValids, head);
             if(tempBest.first > bestWord.first)
             {
                 bestWord = tempBest;
@@ -184,6 +339,7 @@ int main(int argc, char** argv) {
     }
     vector<vector<Cell>> board = convertBoard(boardStr);
     unordered_set<string> dictionary;
+    Trie* head = new Trie();
     ifstream ifs("static/files/diction.txt");
     if(!ifs)
     {
@@ -194,8 +350,9 @@ int main(int argc, char** argv) {
     while(ifs >> word)
     {
         dictionary.insert(word);
+        head->insert(word);
     }
-    auto result = solver(board, dictionary);
+    auto result = solver(board, dictionary, head);
     cout << result.second << ' ' << result.first << endl;
 }
 
